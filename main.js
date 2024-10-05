9d8f3f2be6631aea84c31252c65862332182b064
Iron Man <narendrannirmal6753@gmail.com>
2024-10-06 02:08:19 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,32 +1,29 @@
 console.log('✅ Server Started...');
 
 // could use pm2
-const { join, dirname } = require('path');
-const { createRequire } = require('module');
-const { fileURLToPath } = require('url');
+const { join } = require('path');
 const { setupMaster, fork } = require('cluster');
 const { watchFile, unwatchFile } = require('fs');
 const { createInterface } = require('readline');
 const yargs = require('yargs');
 
-const __dirname = dirname(fileURLToPath(import.meta.url));
+//const __dirname = dirname(fileURLToPath(import.meta.url));
 
 var isRunning = false;
 
 function start(file) {
   if (isRunning) return;
   isRunning = true;
-  
-  let args = [join(__dirname, file), ...process.argv.slice(2)];
+  var args = [join(__dirname, file), ...process.argv.slice(2)];
   console.log(`Starting: ${process.argv[0]} ${args.join(' ')}`);
 
   setupMaster({
     exec: args[0],
     args: args.slice(1),
   });
-  
+
   let p = fork();
-  
+
   p.on('message', data => {
     console.log('[RECEIVED]', data);
     switch (data) {
@@ -45,7 +42,7 @@ function start(file) {
     isRunning = false;
     console.error('❎ An error occurred:', code);
     if (code === 0) return;
-    
+
     watchFile(args[0], () => {
       unwatchFile(args[0]);
       start(file);
